e3ac951ce11bf293feeac981bd1af731ebbb2aea
marlyne26 <marlynebinan@gmail.com>
2024-02-13 23:09:00 +0530

    k

--- app/examples/example.js
+++ app/examples/example.js
@@ -3,3 +3,18 @@ function test(){
 }
 
 test();
+
+let a = 2;
+let b = 3;
+let c = 1;
+let d = 1;
+let k= sum(a,b,c,d);
+console.log(k)
+
+
+function sum(a,b,c,d){
+    let z = a + b + c + d;
+    return z;
+}
+
+
