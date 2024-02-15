a37d6d1d5aaca659c645a71c7dc4a744bd8dc109
marlyne26 <marlynebinan@gmail.com>
2024-02-15 15:23:09 +0530

    h

--- app/modules/loan_mgmt/views/loanRequest.php
+++ app/modules/loan_mgmt/views/loanRequest.php
@@ -20,26 +20,6 @@
 
             <div class="card-body">
               <div id="example2_wrapper" class="dataTables_wrapper dt-bootstrap4">
-                <!-- Button trigger modal -->
-
-                  <!-- Modal -->
-                  <div class="modal fade" id="GrantModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                      <div class="modal-content">
-                        <div class="modal-header">
-                          <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                          ...
-                        </div>
-                        <div class="modal-footer">
-                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                          <button type="button" class="btn btn-primary">Save changes</button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
                 <div class="row">
                   <div class="col-sm-12 col-md-6"></div>
                   <div class="col-sm-12 col-md-6"></div>
@@ -66,7 +46,7 @@
                           <td>1.7</td>
                           <td>20</td>
                           <!-- New button in the "Action" column -->
-                          <td><a href="#" data-bs-toggle="modal" data-bs-target="#GrantModalass" class="btn btn-primary"><button>Grant</button></a></td>
+                          <td><a href="loan-checkLoanRequest"><button class="check-button">Check</button></a></td>
                         </tr>
                         <tr class="even">
                           <td class="dtr-control sorting_1" tabindex="0">Education</td>
@@ -75,8 +55,8 @@
                           <td>1.8</td>
                           <td>20</td>
                           <!-- New button in the "Action" column -->
-                          <td>
-                        </tr><a href="loan-checkLoanRequest"><button class="check-button">Grant</button></a></td>
+                          <td><a href="loan-checkLoanRequest"><button class="check-button">Check</button></a></td>
+                        </tr>
                         <tr class="odd">
                           <td class="dtr-control sorting_1" tabindex="0">Land</td>
                           <td>5</td>
@@ -84,7 +64,7 @@
                           <td>1.8</td>
                           <td>20</td>
                           <!-- New button in the "Action" column -->
-                          <td><a href="loan-checkLoanRequest"><button class="check-button">Grant</button></a></td>
+                          <td><a href="loan-checkLoanRequest"><button class="check-button">Check</button></a></td>
                         </tr>
                         <!-- Add more records as needed -->
                         <tr class="even">
